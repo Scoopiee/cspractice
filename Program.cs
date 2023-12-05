c1391198fe1000bbfddf4f4eb04d6b6c5f15cd91
Aidan McCarthy <aidanmccarthy614@outlook.com>
2023-12-05 06:05:52 +0000

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
+
+namespace Giraffe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+        }
+    }
+} 
